1fd5e6063de5aaa6db2d553c2fd3a83fbc7a5997
“Vidit <vidit.21102@gmail.com>
2022-06-05 14:38:57 +0530

    make it better

---
+++ src/main/java/com/controller/BookController.java
@@ -0,0 +1,48 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bean.BookBean;
+import com.dao.BookDao;
+
+@RestController
+@CrossOrigin
+public class BookController {
+	
+	@Autowired
+	BookDao bookDao;
+	
+	@PostMapping("/book")
+	public BookBean addBook(BookBean book) {
+		bookDao.addBook(book);
+		return book;
+	}
+	
+	@GetMapping("/books")
+	public List<BookBean> getAllBooks(){
+		return bookDao.getAllBooks();
+	}
+	
+	@DeleteMapping("/book/{bookId}")
+	public List<BookBean> deleteBook(@PathVariable("bookId") int bookId){
+		bookDao.deleteBookById(bookId);
+		return bookDao.getAllBooks();
+	}
+	
+	@PutMapping("/book")
+	public BookBean updateBook(BookBean book) {
+		System.out.println(book.getBookId());
+		bookDao.updateBook(book);
+		return book;
+	}
+
+}
---
+++ src/main/java/com/dao/BookDao.java
@@ -0,0 +1,34 @@
+package com.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.bean.BookBean;
+
+@Repository
+public class BookDao {
+
+	@Autowired
+	JdbcTemplate stmt;
+
+	public void addBook(BookBean book) {
+		stmt.update("insert into books (name,author,category,price) values (?,?,?,?)", book.getName(), book.getAuthor(),
+				book.getCategory(), book.getPrice());
+	}
+	
+	public List<BookBean> getAllBooks(){
+		return stmt.query("select * from books", new BeanPropertyRowMapper<>(BookBean.class));
+	}
+
+	public void deleteBookById(int bookId) {
+		stmt.update("delete from books where bookid = ?",bookId);
+	}
+	
+	public void updateBook(BookBean book) {
+		stmt.update("update books set price = ? where bookId = ? ",book.getPrice() , book.getBookId());
+	}
+}
